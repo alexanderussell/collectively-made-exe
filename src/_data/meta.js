8cec58a2ee70312e3a3ea351bcb6534f1337719a
Alex <3697425+alexanderussell@users.noreply.github.com>
2023-09-19 22:46:04 +0000

    Sep 19, 2023, 5:46 PM

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,24 +1,24 @@
 module.exports = {
   url: process.env.URL || 'http://localhost:8080',
-  siteName: 'Eleventy Excellent',
+  siteName: 'Collectively Made',
   siteDescription:
-    'Eleventy starter based on the workflow suggested by buildexcellentwebsit.es.',
-  siteType: 'Person', // schema
+    'Portfolio website for the design collective, Collectively Made.',
+  siteType: 'Agency', // schema
   locale: 'en_EN',
   lang: 'en',
   skipContent: 'Skip to content',
-  author: 'Lene Saile', // i.e. Lene Saile - author's name. Must be set.
-  authorEmail: '', // i.e. hola@lenesaile.com - email of the author
-  authorWebsite: '', // i.e. https.://www.lenesaile.com - the personal site of the author
-  themeColor: '#DD4462', //  Manifest: defines the default theme color for the application
+  author: 'Alex Russell',
+  authorEmail: 'alex@collectivelymade.com', 
+  authorWebsite: 'http://alexanderussell.org', 
+  themeColor: '#232323',
   themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
     opengraph_default: '/assets/images/opengraph-default.jpg', // fallback/default meta image
     opengraph_default_alt:
       'Visible content: Eleventy starter based on workflow for Cube CSS, Every Layout, Design Tokens and Tailwind for uitility, based on the concepts explained in buildexcellentwebsit.es ', // alt text for default meta image
     twitterSite: '', // i.e. @site - twitter profile of the site
-    twitterCreator: '', // i.e. @author -  twitter profile of the site
-    mastodonProfile: '' // i.e. https://front-end.social/@lene - url to your mastodon instance/profile
+    twitterCreator: '@alexanderussell', // i.e. @author -  twitter profile of the site
+    mastodonProfile: '' 
   },
   blog: {
     // this is for the rss feed
@@ -31,14 +31,14 @@ module.exports = {
   },
   address: {
     // edit all presets or leave empty. They are being used in the pages for privacy.md and imprint.md
-    firma: 'Organization name',
-    street: '123 Main St.',
-    city: 'Ciudad',
-    state: 'Estado',
-    zip: '12345',
-    mobileDisplay: '+34 1234567',
-    mobileCall: ' +341234567',
-    email: 'hola@yoursite.com',
+    firma: 'Collectively Made',
+    street: '',
+    city: '',
+    state: '',
+    zip: '',
+    mobileDisplay: '',
+    mobileCall: '',
+    email: 'alex@collectivelymade.com',
     cif: ''
   },
   menu: {
